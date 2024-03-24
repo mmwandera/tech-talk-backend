d52e744ae64efffc16af01157d9ae771cfc594c5
Joan <joanmuhingo@gmail.com>
2024-03-24 11:37:08 +0300

    created seed, delete, query scripts

---
+++ server/delete.py
@@ -0,0 +1,29 @@
+from app import app, db
+from models import User, BlogPost, Comment, Like, followers
+
+def delete_data():
+    print("🗑️ Deleting existing data...")
+    with app.app_context():
+        try:
+            # Delete all data from tables
+            db.session.query(User).delete()
+            db.session.query(BlogPost).delete()
+            db.session.query(Comment).delete()
+            db.session.query(Like).delete()
+
+            # Delete all data from followers table
+            db.session.execute(followers.delete())
+
+            # Commit the changes
+            db.session.commit()
+            print("✅ Existing data deleted.")
+        except Exception as e:
+            # Rollback changes if an error occurs
+            db.session.rollback()
+            print(f"❌ An error occurred while deleting data: {str(e)}")
+        finally:
+            # Close the session
+            db.session.close()
+
+if __name__ == "__main__":
+    delete_data()
